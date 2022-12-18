f986cd53806bf48f58810bd8a02147a491d4ffeb
itamarg84 <118188949+itamarg84@users.noreply.github.com>
2022-12-18 18:03:35 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -8,7 +8,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello World! from itamar and ninahhhhhgfgfg'
+        return 'Hello World! from itamar '
 
 api.add_resource(Greeting, '/') # Route_1
 
